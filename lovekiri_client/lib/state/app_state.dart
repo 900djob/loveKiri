e7d05f52ef48b38ed516aabc187e234f5e2640b8
900djob <900djob@naver.com>
2023-03-30 15:00:46 +0900

    app state user info 추가

--- lovekiri_client/lib/state/app_state.dart
+++ lovekiri_client/lib/state/app_state.dart
@@ -2,21 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:lovekiri_client/models/s_user.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 enum AuthStatus {
-  notDetermined,
+  none,
   loggedIn,
   loggedOut,
 }
 
 class AppState extends GetxController {
-  AuthStatus status = AuthStatus.notDetermined;
+  AuthStatus status = AuthStatus.none;
+
   late SharedPreferences _prefs;
   SharedPreferences get prefs => _prefs;
 
   late LatLng _currentLocation;
   LatLng get currentLocation => _currentLocation;
+
+  late SUser _user;
+  SUser get user => _user;
 
   @override
   void onInit() {
